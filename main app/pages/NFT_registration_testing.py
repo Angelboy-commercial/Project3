7228bdf1515715a5b9209130449a5d578ffd8780
unknown <navcommercial@gmail.com>
2022-08-05 19:08:56 -0400

    Update crowdsale page

--- main app/pages/NFT_registration_testing.py
+++ main app/pages/NFT_registration_testing.py
@@ -1,20 +1,39 @@
 from lib2to3.pgen2 import token
 import os
 import json
+from click import File
 import requests
 from unicodedata import name
 from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
+import psycopg2
+import psycopg2.extras
 
 
+conn = psycopg2.connect(
+    host='localhost',
+    dbname='suppliers',
+    user='postgres',
+    password='Theblockchainpapi34!',
+    port=5432  
+)
+curse = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+insert_event = 'insert into NFTable(nft_id,nft_name,nft_uri,nft_file) values (%s,%s,%s,%s)'
+
+def enter_event(nft_id,nft_name,nft_uri,nft_file):
+    event_confirm = (nft_id,nft_name,nft_uri,nft_file)
+    curse.execute(insert_event, event_confirm)
+    conn.commit()
+    curse.close()
+    conn.close()
 
 #contract = Path("../Project_3/contracts.py")
 pinata = Path("../Project_3/pinata.py")
 #from registerNFT import *
 
-audio = Path("../Project_3/test_music/audio.py")
 load_dotenv()
   
 # Define and connect a new Web3 provider
@@ -27,7 +46,7 @@ def load_contract(file):
     with open(Path(f"../contracts_json/{file}")) as f:
         artwork_abi = json.load(f)
 
-    contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+    contract_address = os.getenv("0x7090172F709a3f85Fc5d3605AbcEcd93C5f1aBBe")
 
     # Load the contract
     contract = w3.eth.contract(
@@ -95,11 +114,11 @@ def pin_appraisal_report(report_content):
     report_ipfs_hash = pin_json_to_ipfs(json_report)
     return report_ipfs_hash
 
-contract = load_contract("crowdfunding.json")
-# w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+contract = load_contract("auctions.json")
+#w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 accounts = w3.eth.accounts
 
-st.title("Register Crowdfund here")
+st.title("Register NFT's Here")
 st.write("Select an account associated with the NFT")
 
 address = st.selectbox("Select Account", options=accounts)
@@ -110,17 +129,20 @@ name_ = st.text_input("Enter the name of the NFT")
 description = st.text_area("Enter a description of the NFT")
 starting_bid = st.text_input("Enter the starting bid")
 #bool
-
+auction_complete = st.checkbox("Auction Completed")
+if auction_complete:
+    st.write("Auction Completed")
 
 end_time = st.number_input("Auction End Time")
-
-
+min_bid = st.number_input("Minimum bid")
+highest_bid = st.number_input("Highest bid")
 
 
 # Use the Streamlit `file_uploader` function create the list of digital image file types(jpg, jpeg, or png) that will be uploaded to Pinata.
 file = st.file_uploader("Upload Artwork", type=["jpg", "jpeg", "png", "mp3", "mp4"])
 
-if st.button("Register Crowdfund NFT"):
+
+if st.button("Register NFT"):
     ipfs_hash, token_json = pin_artwork(name_, file)
     uri = f"ipfs://{ipfs_hash}"
     print(name_, description, starting_bid, uri)
@@ -128,45 +150,15 @@ if st.button("Register Crowdfund NFT"):
         name_,
         description,
         address,
+        int(end_time),
         uri
     ).transact({'from': address, 'gas': 1000000})
     receipt = w3.eth.getTransactionReceipt(tx_hash)
     st.write("Transaction receipt mined:")
     st.write(dict(receipt))
-
-
-
-st.write("---")
-
-if st.button("Donate here"):
-    token_id = st.text_input('Enter the Token Id here')
-    ipfs_hash, token_json = pin_artwork(name_, file)
-    uri = f"ipfs://{ipfs_hash}"
-    print(name_, description, starting_bid, uri)
-    tx_hash = contract.functions.donate(
-        token_id 
-    ).transact({'from': address, 'gas': 1000000})
-    receipt = w3.eth.getTransactionReceipt(tx_hash)
-
-
-
-if st.button('View Top Donors'):
-    pass
-
-
-
-st.write("---")
-
-
+    st.write("You can view the pinned metadata file with the following IPFS Gateway Link")
+    st.markdown(f"[Artwork IPFS Gateway Link](https://ipfs.io/ipfs/{ipfs_hash})")
+    st.markdown(f"[Artwork IPFS Image Link](https://ipfs.io/ipfs/{token_json['image']})")
+    enter_event(1,name_,uri, file)
 
     
-if st.button('View Crowdsale Nfts here'):
-    token_type = st.selectbox('File type', ['mp3', 'image'])
-    token_id = st.text_input('Enter the Token Id here')
-    if token_type == 'mp3':
-
-        audio.song_query(token_id)
-    else:
-        audio.image_query(token_id)
-
-
