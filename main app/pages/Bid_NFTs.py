61ac9b6120bd2ffb1af1e4ef630c7e9018b916ad
kxh3 <kxhoxhi@gmail.com>
2022-08-05 22:37:32 -0400

    made some changes in bidding file

--- main app/pages/Bid_NFTs.py
+++ main app/pages/Bid_NFTs.py
@@ -7,9 +7,14 @@ from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
+from bip44 import Wallet
+from web3 import Account
+from web3 import middleware
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+#crypto_wallet = Path("../main app/crypto_wallet.py")
 
 #contract = Path("../Project_3/contracts.py")
-pinata = Path("../Project_3/pinata.py")
+pinata = Path("../../pinata.py")
 #from registerNFT import *
 
 load_dotenv()
@@ -92,6 +97,65 @@ def pin_appraisal_report(report_content):
     report_ipfs_hash = pin_json_to_ipfs(json_report)
     return report_ipfs_hash
 
+# Create a function called `generate_account` that automates the Ethereum
+# account creation process
+def generate_account(w3):
+    """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
+    # Access the mnemonic phrase from the `.env` file
+    mnemonic = os.getenv("MNEMONIC")
+
+    # Create Wallet object instance
+    wallet = Wallet(mnemonic)
+
+    # Derive Ethereum private key
+    private, public = wallet.derive_account("eth")
+
+    # Convert private key into an Ethereum account
+    account = Account.privateKeyToAccount(private)
+
+    # Return the account from the function
+    return account
+
+# Create a function called `get_balance` that calls = converts the wei balance of the account to ether, and returns the value of ether
+def get_balance(w3, address):
+    """Using an Ethereum account address access the balance of Ether"""
+    # Get balance of address in Wei
+    wei_balance = w3.eth.get_balance(address)
+
+    # Convert Wei value to ether
+    ether = w3.fromWei(wei_balance, "ether")
+
+    # Return the value in ether
+    return ether
+
+# Create a function called `send_transaction` that creates a raw transaction, signs it, and sends it. Return the confirmation hash from the transaction
+def send_transaction(w3, account, receiver, ether):
+    """Send an authorized transaction."""
+    # Set a medium gas price strategy
+    w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
+
+    # Convert eth amount to Wei
+    wei_value = w3.toWei(ether, "ether")
+
+    # Calculate gas estimate
+    gas_estimate = w3.eth.estimateGas({"to": receiver, "from": account.address, "value": wei_value})
+
+    # Construct a raw transaction
+    raw_tx = {
+        "to": receiver,
+        "from": account.address,
+        "value": wei_value,
+        "gas": gas_estimate,
+        "gasPrice": 0,
+        "nonce": w3.eth.getTransactionCount(account.address)
+    }
+
+    # Sign the raw transaction with ethereum account
+    signed_tx = account.signTransaction(raw_tx)
+
+    # Send the signed transactions
+    return w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
 contract = load_contract("auctions.json")
 #w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 accounts = w3.eth.accounts
@@ -103,7 +167,7 @@ nft_database = {
 nfts = ['NFT1', 'NFT2']
 
 address = st.selectbox("Select Account", options=accounts)
-nfts_options = st.selectbox("Select NFTS", options=[0])
+nfts_options = st.selectbox("Select NFTS", options=[int(0)])
 bid = st.text_input("Enter your bid")
 
 # def get_nft(number):
@@ -121,12 +185,17 @@ bid = st.text_input("Enter your bid")
 # for value in (range(len(nfts_options))):
 
 #     get_nft(value)
+contract_address2 = os.getenv("SMART_CONTRACT_ADDRESS")
+account = generate_account(w3)
 
 if st.button("Place Bid"):
     tx_hash = contract.functions.placeBid(nfts_options, int(bid)).transact({"from" : address, 'gas' : 3000000})
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    # ether = st.text_input("Input amount of ether")
+    # trans = send_transaction(w3, account, contract_address2, int(ether))
     st.write("Transaction receipt mined:")
     st.write(dict(receipt))
+    #st.write(dict(trans))
 
 if st.button("Withdraw"):
     tx_hash = contract.functions.withdraw(1)
